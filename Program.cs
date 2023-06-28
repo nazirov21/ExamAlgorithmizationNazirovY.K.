fb8059b86a72a8a743b9ad247e77d4cd642b52ac
isssmmmaaa <113089463+nazirov21@users.noreply.github.com>
2023-06-28 10:21:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            Random random = new Random();
+            for (int i = 0; i < 3 ; i++)
+            {
+                int[] programNumbers = new int[3];
+                for (int j = 0; j < programNumbers.Length; j++)
+                {
+                    programNumbers[j] = random.Next(1, 25);
+
+                }
+                Console.WriteLine("числа программы");
+                for (int j = 0; j < programNumbers.Length; j++)
+                {
+                    Console.WriteLine(programNumbers[j] + "");
+                }
+                Console.WriteLine();
+
+                Console.WriteLine("Введите число от 1 до 24");
+                int userNumber=int.Parse(Console.ReadLine());
+                if (Array.Exists(programNumbers, number => number == userNumber))
+                {
+                    score++;
+
+                }
+
+         
+            }
+            if (score >=2)
+            {
+                Console.WriteLine("вин");
+
+            }
+            else
+            {
+
+                Console.WriteLine("проигрыш");
+            }
+           
+        }
+
+    }
+}
